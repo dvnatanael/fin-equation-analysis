f3c3f3636147048259ddace9dbae09a85828b59c
David Natanael <85625605+dvnatanael@users.noreply.github.com>
2023-06-07 22:58:19 +0800

    feat: add `Rectangle` cross section

--- fin_equation_analysis/fin/cross_section.py
+++ fin_equation_analysis/fin/cross_section.py
@@ -21,6 +21,19 @@ class Square(CrossSection):
         return 4 * np.sqrt(Ac)
 
 
+class RectangleMeta(type):
+    def __init__(self, name: str, w: float) -> None:
+        super().__init__(self)
+
+    def __new__(cls, name: str, w: float):
+        kls = super().__new__(cls, name, (), {"w": w, "P": RectangleMeta.P})
+        return kls
+
+    @staticmethod
+    def P(self, Ac: np_arr_f64) -> np_arr_f64:
+        return np.full_like(Ac, 2 * self.w)
+
+
 class EquilateralTriangle(CrossSection):
     def P(self, Ac: np_arr_f64) -> np_arr_f64:
         return 6 * np.sqrt(Ac) / 3**0.25
