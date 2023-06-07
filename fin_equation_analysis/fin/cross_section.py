4494d24a77d378dc8c01bafd32b64dd40c1cc6c6
David Natanael <85625605+dvnatanael@users.noreply.github.com>
2023-06-07 23:13:06 +0800

    feat: add `Pentagon` cross section

--- fin_equation_analysis/fin/cross_section.py
+++ fin_equation_analysis/fin/cross_section.py
@@ -25,7 +25,7 @@ class RectangleMeta(type):
     def __init__(self, name: str, w: float) -> None:
         super().__init__(self)
 
-    def __new__(cls, name: str, w: float):
+    def __new__(cls, name: str, w: float) -> type:
         kls = super().__new__(cls, name, (), {"w": w, "P": RectangleMeta.P})
         return kls
 
@@ -37,6 +37,11 @@ class RectangleMeta(type):
 class EquilateralTriangle(CrossSection):
     def P(self, Ac: np_arr_f64) -> np_arr_f64:
         return 6 * np.sqrt(Ac) / 3**0.25
+
+
+class RegularPentagon(CrossSection):
+    def P(self, Ac: np_arr_f64) -> np_arr_f64:
+        return 2 * np.sqrt(5 * np.tan(np.pi / 5) * Ac)
 
 
 class RegularHexagon(CrossSection):
