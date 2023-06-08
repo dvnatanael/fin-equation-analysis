8e8a3aa0acfba3112ef63076bfabd1af30d3ea50
David Natanael <85625605+dvnatanael@users.noreply.github.com>
2023-06-09 00:29:20 +0800

    feat!: convert rectangle metaclass to normal class

--- fin_equation_analysis/fin/cross_section.py
+++ fin_equation_analysis/fin/cross_section.py
@@ -16,15 +16,10 @@ class CircleCrossSection(CrossSection):
         return 2 * np.sqrt(np.pi * np.abs(Ac))
 
 
-class RectangleCrossSectionMeta(type):
-    def __init__(self, name: str, w: float) -> None:
-        super().__init__(self)
+class RectangleCrossSection(CrossSection):
+    def __init__(self, w: float) -> None:
+        self.w = w
 
-    def __new__(cls, name: str, w: float) -> type:
-        kls = super().__new__(cls, name, (), {"w": w, "P": RectangleCrossSectionMeta.P})
-        return kls
-
-    @staticmethod
     def P(self, Ac: np_arr_f64) -> np_arr_f64:
         return np.full_like(Ac, 2 * self.w)
 
